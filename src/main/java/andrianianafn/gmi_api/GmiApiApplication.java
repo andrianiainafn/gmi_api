15d9100fa005994c55413d9ae011654d67401003
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-13 23:22:09 +0300

    first commit

---
+++ src/main/java/andrianianafn/gmi_api/GmiApiApplication.java
@@ -0,0 +1,14 @@
+package andrianianafn.gmi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmiApiApplication.class, args);
+    }
+
+}
+    
---
+++ src/test/java/andrianianafn/gmi_api/GmiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package andrianianafn.gmi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
