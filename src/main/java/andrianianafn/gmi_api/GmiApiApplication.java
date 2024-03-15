2c23ff102f2a0660c8f812dc0b231d8a0edf69c8
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-15 21:45:02 +0300

    feat  : setup configuration

--- src/main/java/andrianianafn/gmi_api/GmiApiApplication.java
+++ src/main/java/andrianianafn/gmi_api/GmiApiApplication.java
@@ -1,14 +1,23 @@
 package andrianianafn.gmi_api;
 
+import andrianianafn.gmi_api.config.RsaKeysConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableConfigurationProperties (RsaKeysConfig.class)
 public class GmiApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GmiApiApplication.class, args);
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
-    
