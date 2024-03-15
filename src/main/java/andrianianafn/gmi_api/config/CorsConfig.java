4b24dd6b17c6d5447e604a1f865dd4cbc3cab67f
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-15 21:44:46 +0300

    feat  : setup configuration

---
+++ src/main/java/andrianianafn/gmi_api/config/CorsConfig.java
@@ -0,0 +1,8 @@
+package andrianianafn.gmi_api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConfig {
+
+}
---
+++ src/main/java/andrianianafn/gmi_api/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package andrianianafn.gmi_api.config;
+
+
+import andrianianafn.gmi_api.service.UserDetailServiceImpl;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    private PasswordEncoder passwordEncoder;
+    private final RsaKeysConfig rsaKeysConfig;
+    private final UserDetailsService userDetailsService;
+
+
+    public SecurityConfig(PasswordEncoder passwordEncoder, RsaKeysConfig rsaKeysConfig, UserDetailsService userDetailsService) {
+        this.passwordEncoder = passwordEncoder;
+        this.rsaKeysConfig = rsaKeysConfig;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailServiceImpl userDetailServiceImpl) {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(passwordEncoder);
+        authProvider.setUserDetailsService(userDetailServiceImpl);
+        return new ProviderManager(authProvider);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth->auth.requestMatchers("/api/auth/**").permitAll())
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(rsaKeysConfig.rsaPublicKey()).privateKey(rsaKeysConfig.rsaPrivateKey()).build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return  new NimbusJwtEncoder(jwkSource);
+    }
+    @Bean
+    JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(rsaKeysConfig.rsaPublicKey()).build();
+    }
+
+}
