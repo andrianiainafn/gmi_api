77412c6ba6f23b8dd9c2303228d1a7a033916140
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-16 00:04:09 +0300

    feat: add aller constructor

---
+++ src/main/java/andrianianafn/gmi_api/controller/RequestController.java
@@ -0,0 +1,9 @@
+package andrianianafn.gmi_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/request")
+public class RequestController {
+}
---
+++ src/main/java/andrianianafn/gmi_api/serviceImpl/AccountServiceImpl.java
@@ -0,0 +1,6 @@
+package andrianianafn.gmi_api.serviceImpl;
+
+import andrianianafn.gmi_api.service.AccountService;
+
+public class AccountServiceImpl implements AccountService {
+}
