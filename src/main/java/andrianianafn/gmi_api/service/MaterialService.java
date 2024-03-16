fdaa5b677cce235d54395832d1551a4ae0145d8c
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-17 00:19:13 +0300

    feat: add list and creation service method for material

--- src/main/java/andrianianafn/gmi_api/service/MaterialService.java
+++ src/main/java/andrianianafn/gmi_api/service/MaterialService.java
@@ -1,4 +1,12 @@
 package andrianianafn.gmi_api.service;
 
+import andrianianafn.gmi_api.dto.request.MaterialRequestDto;
+import andrianianafn.gmi_api.entity.Material;
+
+import java.util.List;
+
 public interface MaterialService {
+    void createNewMaterial(MaterialRequestDto materialRequestDto);
+
+    List<Material> getMaterialList(String status);
 }
---
+++ src/main/java/andrianianafn/gmi_api/serviceImpl/MaterialServiceImpl.java
@@ -0,0 +1,43 @@
+package andrianianafn.gmi_api.serviceImpl;
+
+import andrianianafn.gmi_api.dto.request.MaterialRequestDto;
+import andrianianafn.gmi_api.entity.Material;
+import andrianianafn.gmi_api.entity.MaterialStatus;
+import andrianianafn.gmi_api.repository.MaterialRepository;
+import andrianianafn.gmi_api.repository.MaterialStatusRepository;
+import andrianianafn.gmi_api.service.MaterialService;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class MaterialServiceImpl implements MaterialService {
+    private final MaterialRepository materialRepository;
+    private final MaterialStatusRepository materialStatusRepository;
+
+    public MaterialServiceImpl(MaterialRepository materialRepository, MaterialStatusRepository materialStatusRepository) {
+        this.materialRepository = materialRepository;
+        this.materialStatusRepository = materialStatusRepository;
+    }
+
+    @Override
+    public void createNewMaterial(MaterialRequestDto materialRequestDto) {
+        MaterialStatus materialStatus = materialStatusRepository.getReferenceById(materialRequestDto.getStatusId());
+        Material material = Material.builder()
+                .materialName(materialRequestDto.getMaterialName())
+                .description(materialRequestDto.getDescription())
+                .serialNumber(materialRequestDto.getSerialNumber())
+                .state(materialRequestDto.getState())
+                .actualStatus(materialStatus.getMaterialStatusName())
+                .materialStatus(materialStatus)
+                .build();
+        materialRepository.save(material);
+    }
+
+    @Override
+    public List<Material> getMaterialList(String status) {
+        return materialRepository.findByActualStatus(status);
+    }
+}
