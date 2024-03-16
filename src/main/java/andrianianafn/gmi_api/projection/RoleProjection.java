04c59e804767c5691e504bfab611ddcccc28ad09
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-16 22:44:08 +0300

    feat: add role projection

---
+++ src/main/java/andrianianafn/gmi_api/projection/RoleProjection.java
@@ -0,0 +1,10 @@
+package andrianianafn.gmi_api.projection;
+
+import andrianianafn.gmi_api.entity.Role;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(types = {Role.class},name = "pr")
+public interface RoleProjection {
+    String geRoleId();
+    String getRoleName();
+}
--- src/main/java/andrianianafn/gmi_api/repository/RoleRepository.java
+++ src/main/java/andrianianafn/gmi_api/repository/RoleRepository.java
@@ -4,6 +4,9 @@ import andrianianafn.gmi_api.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface RoleRepository extends JpaRepository<Role,String> {
+    List<Role> findAllByRoleIdIsIn(List<String> roles);
 }
