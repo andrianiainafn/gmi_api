fc74cb05ece1f46086a43b6ffc90d974108dbe40
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-17 00:18:52 +0300

    feat: add list and creation endpoints for material

--- src/main/java/andrianianafn/gmi_api/controller/MaterialController.java
+++ src/main/java/andrianianafn/gmi_api/controller/MaterialController.java
@@ -1,9 +1,32 @@
 package andrianianafn.gmi_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import andrianianafn.gmi_api.dto.request.MaterialRequestDto;
+import andrianianafn.gmi_api.entity.Material;
+import andrianianafn.gmi_api.service.MaterialService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/material")
 public class MaterialController {
+
+    private final MaterialService materialService;
+
+    public MaterialController(MaterialService materialService) {
+        this.materialService = materialService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createNewMaterial(@RequestBody MaterialRequestDto materialRequestDto){
+        materialService.createNewMaterial(materialRequestDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Material>> getMaterialList(@RequestParam String status){
+        return new ResponseEntity<>(materialService.getMaterialList(status),HttpStatus.OK);
+    }
 }
