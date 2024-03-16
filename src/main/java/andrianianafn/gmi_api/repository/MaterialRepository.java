90d52b364e7e7bf97e43ce84ff96589d4491d910
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-17 00:20:02 +0300

    feat: add filter by status for material list

--- src/main/java/andrianianafn/gmi_api/repository/MaterialRepository.java
+++ src/main/java/andrianianafn/gmi_api/repository/MaterialRepository.java
@@ -4,6 +4,9 @@ import andrianianafn.gmi_api.entity.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface MaterialRepository extends JpaRepository<Material,String> {
+    List<Material> findByActualStatus(String actualStatus);
 }
