043f5ca94b49806132aa90c3ed891bf772529cb7
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-20 13:38:38 +0300

    fix: cors origin  error

--- src/main/java/andrianianafn/gmi_api/controller/RequestController.java
+++ src/main/java/andrianianafn/gmi_api/controller/RequestController.java
@@ -4,12 +4,14 @@ import andrianianafn.gmi_api.dto.response.RequestStatDto;
 import andrianianafn.gmi_api.service.RequestService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/request")
+@CrossOrigin ("*")
 public class RequestController {
     private final RequestService requestService;
 
