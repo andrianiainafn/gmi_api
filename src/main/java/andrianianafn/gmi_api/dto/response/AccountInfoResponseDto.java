39334cf5c45f0db5f807b81466b76f690253a534
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-04-12 11:20:54 +0300

    feat: add role property to account info

--- src/main/java/andrianianafn/gmi_api/dto/response/AccountInfoResponseDto.java
+++ src/main/java/andrianianafn/gmi_api/dto/response/AccountInfoResponseDto.java
@@ -2,10 +2,13 @@ package andrianianafn.gmi_api.dto.response;
 
 import andrianianafn.gmi_api.entity.Account;
 import andrianianafn.gmi_api.entity.Department;
+import andrianianafn.gmi_api.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,6 +20,7 @@ public class AccountInfoResponseDto {
     private String lastname;
     private String email;
     private String profileUrl;
+    private List<Role> roles;
     private Department department;
 
     public static AccountInfoResponseDto fromAccount(Account account){
@@ -27,6 +31,7 @@ public class AccountInfoResponseDto {
                 .profileUrl(account.getProfileUrl())
                 .firstname(account.getFirstname())
                 .department(account.getDepartment())
+                .roles(account.getRoles())
                 .build();
     }
 }
