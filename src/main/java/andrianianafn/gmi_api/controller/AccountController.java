4b7331b058a4d07ff55536f7f81321dd2430cf60
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-04-14 21:36:02 +0300

    feat : add  functionality to find user by name or email

--- src/main/java/andrianianafn/gmi_api/controller/AccountController.java
+++ src/main/java/andrianianafn/gmi_api/controller/AccountController.java
@@ -1,16 +1,17 @@
 package andrianianafn.gmi_api.controller;
 
 import andrianianafn.gmi_api.dto.request.AccountRequestDto;
+import andrianianafn.gmi_api.dto.response.AccountInfoResponseDto;
 import andrianianafn.gmi_api.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/account")
+@CrossOrigin(origins = "*")
 public class AccountController {
 
     private final AccountService accountService;
@@ -23,6 +24,11 @@ public class AccountController {
     public ResponseEntity<String> createNewAccount(@RequestBody AccountRequestDto accountRequestDto){
         accountService.createNewAccount(accountRequestDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{email-or-name}")
+    public ResponseEntity<List<AccountInfoResponseDto>> searchAccount(@PathVariable ("email-or-name") String emailOrName){
+        return  new ResponseEntity<>(accountService.getAccountByEmailOrName(emailOrName),HttpStatus.OK);
     }
 
 }
